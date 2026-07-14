0dc64a34705f6aad1d6cb89ca741714b6732d647
Ben <meowcat@meowcat.site>
2026-07-14 17:23:09 +0100

    getting init set up

--- src/main/java/org/borealos/App.java
+++ src/main/java/org/borealos/App.java
@@ -1,9 +1,10 @@
 package org.borealos;
 
-
+import org.borealos.pages.WelcomePage;
 
 public class App {
     public static void main(String[] args) {
-
+        org.borealos.pages.WelcomePage welcomePage = new WelcomePage();
+        welcomePage.init();
     }
 }
--- src/main/java/org/borealos/pages/WelcomePage.java
+++ src/main/java/org/borealos/pages/WelcomePage.java
@@ -1,4 +1,28 @@
 package org.borealos.pages;
 
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 public class WelcomePage {
+
+    private Terminal terminal = null;
+
+    public void init() {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        try {
+            this.terminal = terminalFactory.createTerminal();
+            terminal.enterPrivateMode(); // this is so we activate a buffer for our app, and can spool text on the term behind
+            DispWelcomePage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void DispWelcomePage() {
+       try {
+
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+    }
 }
