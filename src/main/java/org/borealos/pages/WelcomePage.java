b6eb13ca32065fdb8ca3765d095d5d737e46a981
Ben <meowcat@meowcat.site>
2026-07-14 17:56:03 +0100

    set some vals as public for easy access

--- src/main/java/org/borealos/pages/WelcomePage.java
+++ src/main/java/org/borealos/pages/WelcomePage.java
@@ -1,17 +1,32 @@
 package org.borealos.pages;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 public class WelcomePage {
 
-    private Terminal terminal = null;
+    public Terminal terminal = null;
+    public Screen screen = null;
+    public TextGraphics graphics = null;
 
     public void init() {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         try {
             this.terminal = terminalFactory.createTerminal();
+            this.screen = new TerminalScreen(terminal);
+            this.graphics = screen.newTextGraphics();
+
+            screen.startScreen();
+            screen.setCursorPosition(null);
+
             terminal.enterPrivateMode(); // this is so we activate a buffer for our app, and can spool text on the term behind
+            terminal.clearScreen();
+
+
             DispWelcomePage();
         } catch (Exception e) {
             e.printStackTrace();
