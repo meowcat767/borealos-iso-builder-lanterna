76cb7e75bdd2251c61b89319dc1f06eadb364152
Ben <meowcat@meowcat.site>
2026-07-17 16:55:41 +0100

    create installconfig

---
+++ src/main/java/org/borealos/val/InstallConfig.java
@@ -0,0 +1,36 @@
+package org.borealos.val;
+
+public class InstallConfig {
+    private String desktopEnvironment;
+
+    private boolean installBash;
+    private boolean installFish;
+    private boolean installSh;
+
+    private boolean kernelLTS;
+    private boolean kernelStd;
+
+    public void setDesktopEnvironment(String desktopEnvironment) {
+        this.desktopEnvironment = desktopEnvironment;
+    }
+
+    public void setInstallBash(boolean installBash) {
+        this.installBash = installBash;
+    }
+
+    public void setInstallFish(boolean installFish) {
+        this.installFish = installFish;
+    }
+
+    public void setInstallSh(boolean installSh) {
+        this.installSh = installSh;
+    }
+
+    public void setKernelLTS(boolean kernelLTS) {
+        this.kernelLTS = kernelLTS;
+    }
+
+    public void setKernelStd(boolean kernelStd) {
+        this.kernelStd = kernelStd;
+    }
+}
